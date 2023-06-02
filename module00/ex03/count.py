f77564db8cf60c172cc387a89c0646276c70ac33
mrakhman <robinbad1312@gmail.com>
2023-06-02 22:34:08 +0200

    ex03

---
+++ module00/ex03/count.py
@@ -0,0 +1,56 @@
+import string
+import sys
+
+
+def text_analyzer(text=""):
+    '''
+    Takes a single string argument 
+    and displays the sums of its upper-case characters, lower-case characters, 
+    punctuation characters and spaces.
+    '''
+
+    if not text:
+        print("What is the text to analyze?")
+        text = input()
+
+    if type(text) != str:
+        print("AssertionError: argument is not a string")
+        return
+
+    upper_case_n = 0
+    lower_case_n = 0
+    punctuation_n = 0
+    spaces_n = 0
+
+    for i in range(0, len(text)):
+        if text[i] == ' ':
+            spaces_n += 1
+
+        elif text[i].islower():
+            lower_case_n += 1
+
+        elif text[i].isupper():
+            upper_case_n += 1
+
+        elif text[i] in string.punctuation:
+            punctuation_n += 1
+
+    res = f"""
+    The text contains {len(text)} character(s):
+    - {upper_case_n} upper letter(s)
+    - {lower_case_n} lower letter(s)
+    - {punctuation_n} punctuation mark(s)
+    - {spaces_n} space(s)
+    """
+
+    print(res)
+
+
+if __name__ == "__main__":
+    args_len = len(sys.argv)
+    if args_len > 2:
+        print("Error: provide only one argument")
+    elif args_len == 1:
+        text_analyzer()
+    else:
+        text_analyzer(sys.argv[1])
