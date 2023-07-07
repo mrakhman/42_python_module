01a674b0011c65381ab91543f2b6a8e37dcae129
mrakhman <robinbad1312@gmail.com>
2023-07-07 19:02:52 +0200

    ex03

--- module00/ex03/count.py
+++ module00/ex03/count.py
@@ -54,3 +54,9 @@ if __name__ == "__main__":
         text_analyzer()
     else:
         text_analyzer(sys.argv[1])
+
+
+# text_analyzer("Python 2.0, released 2000, introduced features like List comprehensions and a garbage collection system capable of collecting reference cycles.")
+# text_analyzer("Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace.")
+# text_analyzer(42)
+# text_analyzer()
