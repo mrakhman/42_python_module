3e25273b761fb001d9bbc850ba28f0aa2ecbd127
Mariia binte RAKHMAN <mrakhman@paul-f3Br10s1.clusters.42paris.fr>
2023-07-11 16:44:20 +0200

    ex00 start

---
+++ module01/ex00/recipe.py
@@ -0,0 +1,9 @@
+class Recipe:
+    def __init__(self, name, cooking_lvl, cooking_time, ingredients, description, recipe_type):
+
+    # name (str): name of the recipe,
+    # cooking_lvl (int): range from 1 to 5,
+    # cooking_time (int): in minutes (no negative numbers),
+    # ingredients (list): list of all ingredients each represented by a string,
+    # description (str): description of the recipe, Csn be empty
+    # recipe_type (str): can be "starter", "lunch" or "dessert".
