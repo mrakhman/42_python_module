dac111d7af3b25b362c1adcaddce2300eeba8b7d
Mariia binte RAKHMAN <mrakhman@paul-f4Br2s1.clusters.42paris.fr>
2023-07-12 12:43:14 +0200

    ex00 not fin

--- module01/ex00/recipe.py
+++ module01/ex00/recipe.py
@@ -7,37 +7,37 @@ class Recipe:
         self.description = description
         self.recipe_type = recipe_type
         
-        @property
-        def name(self):
-            return self._name
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        try:
+            self._name = str(value)
+        except ValueError:
+            raise ValueError("name must be a string")
         
-        @name.setter
-        def name(self, value):
-            try:
-                self._name = str(value)
-            except ValueError:
-                raise ValueError("name must be a string") from None
-            
-        @property
-        def cooking_lvl(self):
-            return self._cooking_lvl
-        
-        @name.setter
-        def cooking_lvl(self, value):
-            if value <= 1 or value >= 5: # not working
-                raise ValueError("kooling_lvl must be from 1 to 5") from None
-            try:
-                self._cooking_lvl = int(value)
-            except ValueError:
-                raise ValueError("name must be a string") from None
+    @property
+    def cooking_lvl(self):
+        return self._cooking_lvl
+    
+    @cooking_lvl.setter
+    def cooking_lvl(self, value):
+        try:
+            int_value = int(value)
+            if int_value < 1 or int_value > 5:
+                raise ValueError("cooking_lvl must be from 1 to 5")
+            self._cooking_lvl = int_value
+        except ValueError:
+            raise ValueError("cooking_lvl must be an integer from 1 to 5")
+    
 
-        
-
-        # def __str__(self):
-        #     """Return the string to print with the recipe info"""
-        #     txt = ""
-        #     """Your code here"""
-        #     return txt
+    # def __str__(self):
+    #     """Return the string to print with the recipe info"""
+    #     txt = ""
+    #     """Your code here"""
+    #     return txt
 
 
     # name (str): name of the recipe,
@@ -46,3 +46,12 @@ class Recipe:
     # ingredients (list): list of all ingredients each represented by a string,
     # description (str): description of the recipe, Csn be empty
     # recipe_type (str): can be "starter", "lunch" or "dessert".
+
+rec = Recipe(6, 8, 4, 3, 2, 1)
+print(rec.name)
+rec.name = 11
+print(rec.name)
+print(rec.cooking_lvl)
+rec.cooking_lvl = 'rerer'
+print(rec.cooking_lvl)
+print(rec.cooking_lvl)
