74452613ddeecf9c0b5569a7f22af1d5eeb29000
Mariia binte RAKHMAN <mrakhman@paul-f4Ar8s6.clusters.42paris.fr>
2023-08-09 18:24:19 +0200

    ex02: fix dot, T, add, sub

--- module01/ex02/test.py
+++ module01/ex02/test.py
@@ -1,1 +1,88 @@
 from vector import Vector
+import unittest
+import numpy as np
+
+
+class TestVector(unittest.TestCase):
+    @unittest.skip # TODO: rm
+    def test_dot_product(self):
+        # Row vector
+        vector1 = Vector([[0., 1., 2.]])
+        vector2 = Vector([[0., 1., 2.]])
+
+        self.assertEqual(vector1.dot(vector2), 5.0)
+        self.assertEqual(vector1.dot(Vector([[0., 1., 2., 3.]])), None) # error - different size
+        self.assertEqual(vector1.dot(Vector([[0.], [1.], [2.]])), None) # error - different shape
+
+        # Column vector
+        vector3 = Vector([[0.], [1.], [2.]])
+        vector4 = Vector([[0.], [1.], [2.]])
+
+        self.assertEqual(vector3.dot(vector4), 5.0)
+        self.assertEqual(vector3.dot(Vector([[0.], [1.], [2.], [3.]])), None) # error - different size
+        self.assertEqual(vector3.dot(Vector([[0., 1., 2.]])), None) # error - different shape
+
+    @unittest.skip # TODO: rm
+    def test_transpose(self):
+        vector1 = Vector([[0., 1., 2.]])
+        vector2 = Vector([[0.], [1.], [2.]])
+
+        self.assertEqual(vector1.T().shape, vector2.shape) # row becomes column
+        self.assertEqual(vector1.T().T().shape, vector1.shape) # transposing twice returns initial vector
+        self.assertEqual(vector2.T().shape, vector1.shape) # column becomes row
+
+    # @unittest.skip # TODO: rm
+    def test_add(self):
+        vector1 = Vector([[0., 1., 2.]])
+        vector2 = Vector([[0., 1., 2.]])
+        vector3 = Vector([[0., 1., 2., 3., 4.]])
+        vector4 = Vector([[0.], [1.], [2.]])
+        vector5 = Vector([[0.], [1.], [2.]])
+
+        self.assertEqual((vector1 + vector2).values, [[0., 2., 4.]])
+        self.assertEqual((vector4 + vector5).values, [[0.], [2.], [4.]])
+        self.assertEqual([1., 3., 0.] + vector2, None) # error - first argument not a Vector
+        self.assertEqual(vector2 + vector3, None) # error - different shapes
+    
+    def test_sub(self):
+        vector1 = Vector([[0., 1., 2.]])
+        vector2 = Vector([[0., 2., 6.]])
+        vector3 = Vector([[0., 1., 2., 3., 4.]])
+        vector4 = Vector([[0.], [1.], [2.]])
+        vector5 = Vector([[3.], [4.], [5.]])
+
+
+        self.assertEqual((vector1 - vector2).values, [[0., -1., -4.]])
+        self.assertEqual((vector4 - vector5).values, [[-3.], [-3.], [-3.]])
+        self.assertEqual([1., 3., 0.] - vector2, None) # error - first argument not a Vector
+        self.assertEqual(vector2 - vector3, None) # error - different shapes
+
+        print()
+        print('asasas', vector1.values)
+        print('asasas', vector4.values)
+        
+        
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
+    a = np.array([[1.], [2.], [3.]])
+    b = np.array([[1.], [8.], [6.]])
+    print(a - b)
+    print()
+    print(a)
+    print()
+
+    print(b)
+    # b = np.array([[1., 2., 3.]])
+    # d = np.array([[1., 2., 3.]])
+    # c = b + b
+    # print(c)
+    # print(a)
+    # print()
+    # print(np.transpose(a))
+    # print()
+    # print(a)
+
+    # print(np.transpose(b))
--- module01/ex02/vector.py
+++ module01/ex02/vector.py
@@ -1,3 +1,4 @@
+import copy
 # Input validation
 def is_valid_column_vector(vector):
     if not isinstance(vector, list) \
@@ -36,7 +37,7 @@ def get_vector_shape(vector):
         return (len(vector), 1)
     if is_valid_row_vector(vector):
         return (1, len(vector[0]))
-    return None
+    return False
 
 def can_generate_vector_from_size(size):
     if isinstance(size, int) and size > 0:
@@ -63,8 +64,19 @@ def get_vector_size(vector):
         return len(vector[0])
     return False
 
+# To prevent throwing error in console
+def _guard_(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return (func(*args, **kwargs))
+        except Exception as e:
+            print(e)
+            return None
+    return wrapper
+
 
 class Vector:
+    @_guard_
     def __init__(self, values):
         if is_valid_vector(values):
             self.values = values
@@ -78,11 +90,62 @@ class Vector:
         else:
             raise ValueError("Error: Invalid values for Vector")
         
+    def __str__(self):
+        txt = ("Vector values: " + str(self.values) + "\nVector shape:  " + str(self.shape))
+        return txt
+
+    def __repr__(self):
+        txt = "Vector" + str(self.values) + "; " + str(self.shape)
+        return txt
+        
+    @_guard_
+    def __add__(self, v2):
+        """Addition of 2 vectors of the same shape"""
+        res = copy.deepcopy(self)
+        if isinstance(v2, Vector) and res.shape == v2.shape:
+            for i in range(len(res.values)):
+                for j in range(len(res.values[0])):
+                    res.values[i][j] += v2.values[i][j]
+            print('4', self.values)
+            return res
+        else:
+            raise ValueError("Error: invalid vector for add operation")
+        
+    @_guard_
+    def __radd__(self, v2):
+        self.__add__(v2)
+
+    @_guard_
+    def __sub__(self, v2):
+        """Substraction of 2 vectors of the same shape"""
+        res = copy.deepcopy(self)
+        if isinstance(v2, Vector) and res.shape == v2.shape:
+            for i in range(len(res.values)):
+                for j in range(len(res.values[0])):
+                    res.values[i][j] -= v2.values[i][j]
+            return res
+        else:
+            raise ValueError("Error: invalid vector for sub operation")
+
+    @_guard_
+    def __rsub__(self, v2):
+        self.__sub__(v2)
+
+    @_guard_
+    def __truediv__(self):
+        """Division of vector and scalar"""
+        pass
+
+    @_guard_
+    def __rtruediv__(self):
+        raise ArithmeticError("Error: can not divide value by vector")
+        
+    @_guard_
     def dot(self, vector2):
-        v1_shape = get_vector_shape(self)
-        v2_shape = get_vector_shape(vector2)
+        v1_shape = get_vector_shape(self.values)
+        v2_shape = get_vector_shape(vector2.values)
         if not v1_shape == v2_shape:
-            raise ValueError("Error: one or both vectors are invalid")
+            raise ValueError("Error: vectors are invalid or not the same dimention")
 
         def multiply_two_column_vectors(v1, v2):
             return [v1[i][0] * v2[i][0] for i, _el in enumerate(v1)]
@@ -98,29 +161,49 @@ class Vector:
         if is_valid_row_vector(self.values) and is_valid_row_vector(vector2.values):
             new_v = multiply_two_row_vectors(self.values, vector2.values)
             return list_sum(new_v)
-        return print("Error: one or both vectors are invalidd")
+        raise ValueError("Error: one or both vectors are invalidd")
+
+    @_guard_
+    def T(self):
+        if is_valid_column_vector(self.values):
+            new_row_v = [[]]
+            for el in self.values:
+                new_row_v[0].append(el[0])
+            return Vector(new_row_v)
+
+        if is_valid_row_vector(self.values):
+            new_col_v = []
+            for el in self.values[0]:
+                new_col_v.append([el])
+            return Vector(new_col_v)
+        
+        raise ValueError("Error: vector can not be transposed")
+
 
         
+if __name__ == '__main__':
+    # pass
 
+    # print(Vector([[1.], [2.], [3.]]).shape)
+    # print(Vector([[[1], [2.], [3.]]])) # not valid
+    # print(Vector([[[1., 1., 1.], [2., 2., 2.], [3., 3., 3.]]])) # not valid
+    # print()
+    # print(Vector([[1., 2., 3.]]).shape)
+    # print(Vector([[1., 2., 3.], [1., 2., 3.]])) # not valid
+    # print(Vector([1., 2., 3.])) # not valid
 
-# print(Vector([[1.], [2.], [3.]]).shape)
-# print(Vector([[[1], [2.], [3.]]])) # not valid
-# print(Vector([[[1., 1., 1.], [2., 2., 2.], [3., 3., 3.]]])) # not valid
-# print()
-# print(Vector([[1., 2., 3.]]).shape)
-# print(Vector([[1., 2., 3.], [1., 2., 3.]])) # not valid
-# print(Vector([1., 2., 3.])) # not valid
+    # v1 = Vector([[1.], [2.], [3.]])
 
-# v1 = Vector([[1.], [2.], [3.]])
+    v2 = Vector(3) # generate vector from value
+    print(v2.shape, v2.values)
 
-v2 = Vector(3) # generate vector from value
-print(v2.shape, v2.values)
+    v3 = Vector((0, 3)) # generate vector from range
+    print(v3.shape, v3.values)
 
-v3 = Vector((0, 3)) # generate vector from range
-print(v3.shape, v3.values)
+    print('column dot:', v2.dot(v3))
 
-print('column dot:', v2.dot(v3))
-
-v4 = Vector([[0., 1., 2.]])
-v5 = Vector([[0., 1., 2.]])
-print('row dot:', v4.dot(v5))
+    v4 = Vector([[0., 1., 2.]])
+    v5 = Vector([[0., 1., 2.]])
+    v6wrong = Vector([[1.], [2.], [3.]])
+    print('row dot:', v4.dot(v5))
+    print('row dot:', v4.dot(v6wrong))
