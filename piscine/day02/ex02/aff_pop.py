0023d3b911a7cd43d783e641d169acae6fa8b7d1
Mariia binte RAKHMAN <mrakhman@paul-f3Br10s1.clusters.42paris.fr>
2023-08-28 18:00:10 +0200

    ex02

---
+++ piscine/day02/ex02/aff_pop.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+from load_csv import load
+
+
+def _guard_(func):
+    '''Prevents throwing error in console'''
+    def wrapper(*args, **kwargs):
+        try:
+            return (func(*args, **kwargs))
+        except Exception as e:
+            print(e)
+            return None
+    return wrapper
+
+
+def normalize_value(val):
+    if val[-1] == 'k':
+        val = int(float(val[:-1]) * 1000)
+        return val
+    if val[-1] == 'M':
+        val = int(float(val[:-1]) * 1000000)
+        return val
+    return int(float(val))
+
+
+def aff_life():
+    '''Display 2 graphics of population: in France and another country'''
+    df = load('../population_total.csv')
+    country2 = 'Belgium'
+
+    # Prepare data
+    new_df = df.loc[df.country.isin([country2, 'France'])].drop(
+        columns=['country']).T
+    new_df = new_df.iloc[:251]  # untill year 2050
+    new_df.columns = [country2, 'France']
+    # years column to type int for x-axis ticks
+    new_df.index = new_df.index.astype(int)
+    new_df['France-int'] = new_df.France.map(normalize_value)
+    country2_col_name = country2 + '-int'
+    new_df[country2_col_name] = new_df[country2].map(normalize_value)
+
+    # pd.set_option('display.max_rows', None)
+    # print(new_df)
+
+    # Set graph title
+    _, ax = plt.subplots()
+    ax.set_title('Population projections')
+
+    # Set axis labels
+    plt.xlabel('Year')
+    plt.ylabel('Population')
+
+    # Set ticks for y-axis
+    plt.yticks(ticks=new_df['France-int'], labels=new_df['France'])
+    plt.locator_params(axis='y', nbins=5)
+
+    # Plot and show
+    plt.plot(new_df['France-int'], label='France')
+    plt.plot(new_df[country2_col_name], label=country2)
+    plt.legend(loc="upper left")
+    plt.show()
+
+
+# @_guard_
+def main():
+    '''Main for tests and error handling'''
+    aff_life()
+
+
+if __name__ == "__main__":
+    main()
---
+++ piscine/day02/ex02/load_csv.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+
+def _guard_(func):
+    '''Prevents throwing error in console'''
+    def wrapper(*args, **kwargs):
+        try:
+            return (func(*args, **kwargs))
+        except Exception as e:
+            print(e)
+            return None
+    return wrapper
+
+
+Dataset = pd.core.frame.DataFrame
+
+
+def load(path: str) -> Dataset:
+    '''Loads and returns dataset from a given path'''
+    try:
+        df = pd.read_csv(path)
+        print('Loading dataset of dimensions', df.shape)
+        return df
+    except Exception:
+        print('Error: Unable to load file')
+        return None
+
+
+@_guard_
+def main():
+    '''Main for tests and error handling'''
+    # print(load('../population_total.csv'))
+    pass
+
+
+if __name__ == "__main__":
+    main()
