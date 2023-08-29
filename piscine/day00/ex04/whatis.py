2ef327fad793faacb7e3048b6d7c5f87b84c258a
Mariia binte RAKHMAN <mrakhman@paul-f4Ar2s1.clusters.42paris.fr>
2023-08-29 16:34:02 +0200

    ex04

---
+++ piscine/day00/ex04/whatis.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def whatis():
+    args_len = len(sys.argv)
+
+    if args_len < 2:
+        return ''  # Error: Add one number
+
+    if args_len > 2:
+        return ('AssertionError: more than one argument is provided')
+
+    num_str = sys.argv[1]
+
+    try:
+        num = int(num_str)
+        if num == 0:
+            return ("I'm Zero.")
+
+        if num % 2 == 0:
+            return ("I'm Even.")
+
+        if num % 2 != 0:
+            return ("I'm Odd.")
+
+    except Exception:
+        return ('Argument should be an integer')
+
+
+print(whatis())
