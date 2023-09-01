98bfe15c3f93faf071deeeb1ea7119a7813ae8e8
Mariia binte RAKHMAN <mrakhman@paul-f4Ar2s3.clusters.42paris.fr>
2023-09-01 18:30:17 +0200

    ex00 wip

--- piscine/day03/ex00/S1E9.py
+++ piscine/day03/ex00/S1E9.py
@@ -32,30 +32,48 @@ def _guard_(func):
 class Character(ABC):
     '''Abstract class. Takes first_name and is_alive \
 and changes the health state of the character'''
+    @abstractmethod
     def __init__(self, first_name, is_alive=True):
-        '''Init '''
-        pass
-        # TODO: here
+        '''Init function to create character'''
+        if is_valid_name(first_name) and is_valid_is_alive(is_alive):
+            self.first_name = first_name
+            self.is_alive = is_alive
+        else:
+            return
 
     @abstractmethod
-    def sss():
-        '''Method that'''
-        pass
+    def die(self):
+        '''Method that kills a character by setting is_alive to false'''
+        self.is_alive = False
 
 
 class Stark(Character):
-    '''Inherots from Character.\
+    '''Inherits from Character. \
 Class representing the Stark family'''
-    def __init__(self, first_name=None, is_alive=True):
-        '''Init '''
-        # TODO: here
-        pass
+    def __init__(self, first_name, is_alive=True):
+        '''Init function to create Stark family character'''
+        super().__init__(first_name=first_name, is_alive=is_alive)
+        # TODO: figure out how to inherit abstract method
+
+
 
 
 @_guard_
 def main():
     '''Main for tests and error handling'''
-    pass
+    # aaa = Character('aa', False)
+    # print(aaa)
+    Ned = Stark("Ned")
+    print(Ned.__dict__)
+    print(Ned.is_alive)
+    Ned.die()
+    print(Ned.is_alive)
+    print(Ned.__doc__)
+    print(Ned.__init__.__doc__)
+    print(Ned.die.__doc__)
+    print("---")
+    Lyanna = Stark("Lyanna", False)
+    print(Lyanna.__dict__)
 
 
 if __name__ == "__main__":
